a0ee0587be4a7f840970866a0aa51fc9c333d02b
Wangcx <90006503+ChunXiangWang@users.noreply.github.com>
2022-01-20 19:47:00 -0500

    Delete java/com/cy/store directory

--- java/com/cy/store/mapper/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cy.store.mapper;
-
-import com.cy.store.entity.Order;
-import com.cy.store.entity.OrderItem;
-
-public interface OrderMapper {
-    Integer insertOrder(Order order);
-    Integer insertOrderItem(OrderItem orderItem);
-}
--- java/com/cy/store/service/IOrderService.java
+++
@@ -1,7 +0,0 @@
-package com.cy.store.service;
-
-import com.cy.store.entity.Order;
-
-public interface IOrderService {
-    Order create(Integer aid, Integer[] cids, Integer uid, String username);
-}
