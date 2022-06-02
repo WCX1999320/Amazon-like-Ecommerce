3e728b1ba18effc10549a9b115f5da48771bcc28
Tommy Wang <tommycx1999@gmail.com>
2022-06-01 22:03:32 -0400

    Update StoreApplication.java

--- src/main/java/com/cy/store/StoreApplication.java
+++ src/main/java/com/cy/store/StoreApplication.java
@@ -3,9 +3,11 @@ package com.cy.store;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.cy.store.mapper")
+@EnableCaching
 public class StoreApplication {
 
     public static void main(String[] args) {
